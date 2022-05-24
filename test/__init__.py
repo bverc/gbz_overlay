3c9fb3308a1d804880ffc0bcb0ed2555c3914dd0
Brendan Vercoelen <bvercoelen@gmail.com>
2022-05-24 23:22:28 +1200

    Added unit tests for devices

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""Test package for retropie-status-overlay
+github.com/bverc/retropie-status-overlay
+
+Author: bverc
+"""
+
+__all__ = ["test_battery", "test_wifi", "test_audio", "test_bluetooth"]
