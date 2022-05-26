bfe5f99132ef2d58d76179a272c982656bac23b2
Brendan Vercoelen <bvercoelen@gmail.com>
2022-05-26 21:28:30 +1200

    fix pijuice unittest

--- test/test_pj.py
+++ test/test_pj.py
@@ -5,22 +5,22 @@ Author: bverc
 """
 
 import unittest
+from unittest.mock import Mock
 
 from adc import pj
 
-class MockPiJuiceStatus(): # pylint: disable=too-few-public-methods
-    """A Class to represent an pijuice.PiJuice.status()"""
+pj.pijuice.status = Mock()
 
-    def GetBatteryVoltage(self): # pylint: disable=invalid-name, no-self-use
-        """A method to mock PiJuice.status.GetBatteryVoltage()"""
-        return {"data": 1100}
+def mock_get_voltage():
+    """A method to mock PiJuice.status.GetBatteryVoltage()"""
+    return {"data": 1100}
 
 class TestPJ(unittest.TestCase):
     """A Class used to test PJ module."""
 
     def test_read(self):
         """Test pj.read()"""
-        pj.pijuice.status = MockPiJuiceStatus()
+        pj.pijuice.status.GetBatteryVoltage.side_effect = mock_get_voltage
         self.assertTrue(pj.read(0) == 1.1)
 
 if __name__ == '__main__':
