3e592016f4236990f541f593333aced3f413eb89
Brendan Vercoelen <bvercoelen@gmail.com>
2022-05-26 20:53:52 +1200

    change ads1015 to use unittest.mock

--- test/test_ads1015.py
+++ test/test_ads1015.py
@@ -5,25 +5,26 @@ Author: bverc
 """
 
 import unittest
+from unittest.mock import Mock
 
 from adc import ads1015
 
-class MockADS1015: # pylint: disable=too-few-public-methods
-    """A Class to represent an Adafruit_ADS1x15.ADS1015()."""
-    def read_adc(self, channel, gain): # pylint: disable=unused-argument, no-self-use
-        """A method to mock ADS1015().read_adc()"""
+ads1015.adc = Mock()
+
+class TestADS1015(unittest.TestCase):
+    """A Class used to test ADC1015 module."""
+    
+    def mock_read_adc(self, channel, gain): 
+        """A function to mock ADS1015().read_adc()"""
         if channel == 0:
             return 0
         if channel == 1:
             return 512
         return 1024
 
-class TestADS1015(unittest.TestCase):
-    """A Class used to test ADC1015 module."""
-
     def test_read(self):
         """Test ads1015.read()"""
-        ads1015.adc = MockADS1015()
+        ads1015.adc.read_adc.side_effect = self.mock_read_adc
         self.assertTrue(ads1015.read(0) == 0)
         self.assertTrue(ads1015.read(1) == 1.536)
         self.assertTrue(ads1015.read(2) == 3.072)
