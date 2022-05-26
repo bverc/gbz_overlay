0e08593d0be80712ee777b71f07653a0265a733f
Brendan Vercoelen <bvercoelen@gmail.com>
2022-05-26 21:56:46 +1200

    Create test_mcp3008.py

---
+++ test/test_mcp3008.py
@@ -0,0 +1,33 @@
+"""Unit tests for adc/mcp3008.py
+github.com/bverc/retropie-status-overlay
+
+Author: bverc
+"""
+
+import unittest
+from unittest.mock import Mock
+
+from adc import mcp3008
+
+mcp3008.adc = Mock()
+
+def mock_read_adc(channel):
+    """A function to mock MCP3008().read_adc()"""
+    if channel == 0:
+        return 0
+    if channel == 1:
+        return 512
+    return 1024
+
+class TestMCP3008(unittest.TestCase):
+    """A Class used to test MCP3008 module."""
+
+    def test_read(self):
+        """Test mcp3008.read()"""
+        mcp3008.adc.read_adc.side_effect = mock_read_adc
+        self.assertTrue(ads1015.read(0) == 0)
+        self.assertTrue(ads1015.read(1) == 1.65)
+        self.assertTrue(ads1015.read(2) == 3.3)
+
+if __name__ == '__main__':
+    unittest.main()
